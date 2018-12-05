0e273693ef91f59da8a6485c0ede94fe3b336573
Charles <liucharlesche@gmail.com>
2018-12-05 14:28:49 +0900

    Fixed something

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -8,7 +8,10 @@ export default class MapScreen extends React.Component {
     this.state = {
       lattitude: 35.708647,
       longitude: 139.729769,
-
+      coordinate: new AnimatedRegion({
+        latitude: 35.708647,
+        longitude: 139.729769
+      }),
     };
   }
   static navigationOptions = {
@@ -17,15 +20,15 @@ export default class MapScreen extends React.Component {
 
 
   render() {
-    const location = navigator.geolocation.getCurrentPosition();
+
     return (
       <View style={styles.container}>
         <MapView
           provider={PROVIDER_GOOGLE} // remove if not using Google Maps
           style={styles.map}
           region={{
-            latitude: location.latitude,
-            longitude: location.longitude,
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
             latitudeDelta: 0.015,
             longitudeDelta: 0.0121,
           }}
