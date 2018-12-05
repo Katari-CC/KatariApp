41f91cbe9cf0898c903cc4bf0947e3f5fc9732d3
Charles <liucharlesche@gmail.com>
2018-12-05 14:26:38 +0900

    Reconfigure gitignore.

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -3,19 +3,29 @@ import { View, Text, StyleSheet } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default class MapScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lattitude: 35.708647,
+      longitude: 139.729769,
+
+    };
+  }
   static navigationOptions = {
     title: 'Map',
   };
 
+
   render() {
+    const location = navigator.geolocation.getCurrentPosition();
     return (
       <View style={styles.container}>
         <MapView
           provider={PROVIDER_GOOGLE} // remove if not using Google Maps
           style={styles.map}
           region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
+            latitude: location.latitude,
+            longitude: location.longitude,
             latitudeDelta: 0.015,
             longitudeDelta: 0.0121,
           }}
