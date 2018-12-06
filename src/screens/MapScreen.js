4442638818976e1fe783d7e05aade855ab652050
Charles <liucharlesche@gmail.com>
2018-12-06 14:37:29 +0900

    Some location marker stuff

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Marker, AnimatedRegion } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker, AnimatedRegion, Circle } from 'react-native-maps';
 
 export default class MapScreen extends React.Component {
   constructor(props) {
@@ -12,31 +12,50 @@ export default class MapScreen extends React.Component {
       {
         latitude: 35.708647,
         longitude: 139.729769
-      }
-      ,
+      },
+      markers: [],
     };
+    this.addMarker = this.addMarker.bind(this);
   }
   static navigationOptions = {
     title: 'Map',
   };
 
+  //Add Marker function we can use later for adding
+  addMarker(e) {
+    this.setState({
+      markers: [
+        ...this.state.markers,
+        {
+          coordinate: e.nativeEvent.coordinate
+        }
+      ],
 
+    })
+  }
   render() {
 
     return (
       <View style={styles.container} >
-        <Text>{JSON.stringify(this.state)}</Text>
         <MapView
           provider={PROVIDER_GOOGLE} // remove if not using Google Maps
           style={styles.map}
           region={{
             latitude: this.state.latitude,
             longitude: this.state.longitude,
-            latitudeDelta: 0.010,
-            longitudeDelta: 0.009,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.007,
           }}
         >
-          <MapView.Marker coordinate={this.state.coordinate} />
+          <MapView.Marker coordinate={this.state.coordinate} >
+            <View style={styles.currentLocation}>
+            </View>
+          </MapView.Marker>
+          {/* {this.state.markers.map((marker) => {
+            return (
+              <Marker {...marker} />
+            )
+          })} */}
         </MapView>
       </View>
     );
@@ -52,4 +71,16 @@ const styles = StyleSheet.create({
   map: {
     ...StyleSheet.absoluteFillObject,
   },
+  currentLocation: {
+    borderRadius: 100,
+    backgroundColor: "#339EFF",
+    padding: 8,
+    borderWidth: 3,
+    borderColor: "#FFF",
+  },
+  marker: {
+    backgroundColor: "#550bbc",
+    padding: 5,
+    borderRadius: 100,
+  }
 });
