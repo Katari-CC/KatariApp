819d65b6384849f7d3e3d80a8389583342fc6ad2
Charles <liucharlesche@gmail.com>
2018-12-19 12:09:38 +0900

    Changed the look for the Signup and Login screen. Added Start screen.

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createSwitchNavigator, createAppContainer } from "react-navigation";
 import Loading from "../authScreen/Loading";
+import Start from "../authScreen/Start";
 import Login from "../authScreen/Login";
 import SignUp from "../authScreen/SignUp";
 import MainTabNavigator from "./MainTabNavigator";
@@ -10,9 +11,10 @@ const AppNavigator = createSwitchNavigator(
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
     Loading,
+    Start,
     Login,
     SignUp,
-    Main: MainTabNavigator
+    Main: MainTabNavigator,
   },
   { initialRouteName: "Loading" }
 );
